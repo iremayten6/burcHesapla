22a566e2f744b7d7ee0ec3d4fcc3b539b8cbed47
İREM AYTEN <126502980+iremayten6@users.noreply.github.com>
2023-05-05 19:37:59 +0300

    Add project files.

---
+++ burcHesapla/Form1.Designer.cs
@@ -0,0 +1,108 @@
+﻿namespace burcHesapla
+{
+    partial class BurcHesapla
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(BurcHesapla));
+            this.TxtBurc = new System.Windows.Forms.TextBox();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.buttonOk = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // TxtBurc
+            // 
+            this.TxtBurc.BackColor = System.Drawing.Color.Beige;
+            this.TxtBurc.ForeColor = System.Drawing.SystemColors.InfoText;
+            this.TxtBurc.Location = new System.Drawing.Point(54, 360);
+            this.TxtBurc.Multiline = true;
+            this.TxtBurc.Name = "TxtBurc";
+            this.TxtBurc.Size = new System.Drawing.Size(264, 54);
+            this.TxtBurc.TabIndex = 0;
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.CalendarForeColor = System.Drawing.SystemColors.ActiveBorder;
+            this.dateTimePicker1.CalendarTrailingForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.dateTimePicker1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.125F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.dateTimePicker1.Location = new System.Drawing.Point(12, 131);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(425, 38);
+            this.dateTimePicker1.TabIndex = 1;
+            // 
+            // buttonOk
+            // 
+            this.buttonOk.BackColor = System.Drawing.Color.Beige;
+            this.buttonOk.Location = new System.Drawing.Point(54, 216);
+            this.buttonOk.Name = "buttonOk";
+            this.buttonOk.Size = new System.Drawing.Size(264, 97);
+            this.buttonOk.TabIndex = 2;
+            this.buttonOk.Text = "OK";
+            this.buttonOk.UseVisualStyleBackColor = false;
+            this.buttonOk.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(447, 57);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(326, 452);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // BurcHesapla
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.ClientSize = new System.Drawing.Size(800, 560);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.buttonOk);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.TxtBurc);
+            this.Name = "BurcHesapla";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "BurcHesapla";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox TxtBurc;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.Button buttonOk;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ burcHesapla/Form1.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace burcHesapla
+{
+    public partial class BurcHesapla : Form
+    {
+        int gun;
+        int ay;
+        public BurcHesapla()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    
+        private void dateTimePicker1_ValueChanged(object sender, EventArgs e)
+        {
+            ay = Convert.ToInt16(dateTimePicker1.Value.Month);
+            gun = Convert.ToInt16(dateTimePicker1.Value.Day);
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            ay = Convert.ToInt16(dateTimePicker1.Value.Month);
+            gun = Convert.ToInt16(dateTimePicker1.Value.Day);
+
+            if ((ay == 12 & gun >= 12) || (ay == 1 & gun <= 20))
+            {
+                TxtBurc.Text = "OĞLAK";
+
+            }
+
+            if ((ay == 9 & gun >= 22) || (ay == 10 & gun <= 23))
+            {
+                TxtBurc.Text = "TERAZİ";
+
+            }
+
+            if ((ay == 1 & gun >= 21) || (ay == 2 & gun <= 19))
+            {
+                TxtBurc.Text = "KOVA";
+
+            }
+
+            if ((ay == 2 & gun >= 20) || (ay == 3 & gun <= 20))
+            {
+                TxtBurc.Text = "BALIK";
+
+            }
+
+            if ((ay == 3 & gun >= 21) || (ay == 4 & gun <= 20))
+            {
+                TxtBurc.Text = "KOÇ";
+
+            }
+
+            if ((ay == 4 & gun >= 21) || (ay == 5 & gun <= 21))
+            {
+                TxtBurc.Text = "BOĞA";
+
+            }
+
+
+            if ((ay == 5 & gun >= 22) || (ay == 6 & gun <= 21))
+            {
+                TxtBurc.Text = "İKİZLER";
+            }
+
+            if ((ay == 6 & gun >= 22) || (ay == 7 & gun <= 23))
+            {
+                TxtBurc.Text = "YENGEÇ";
+
+            }
+
+            if ((ay == 7 & gun >= 24) || (ay == 8 & gun <= 23))
+            {
+                TxtBurc.Text = "ASLAN";
+
+            }
+
+            if ((ay == 8 & gun >= 24) || (ay == 9 & gun <= 23))
+            {
+                TxtBurc.Text = "BAŞAK";
+            }
+
+
+            if ((ay == 102 & gun >= 23) || (ay == 11 & gun <= 22))
+            {
+                TxtBurc.Text = "AKREP";
+            }
+
+            if ((ay == 11 & gun >= 23) || (ay == 12 & gun <= 22))
+            {
+                TxtBurc.Text = "YAY";
+            }
+        }
+    }
+}
